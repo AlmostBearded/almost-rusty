3515a381e397d3d0d304ded4d0c8db03f9a0b0ad
Peter Oberrauner <thealmostbearded@gmail.com>
2020-02-05 15:42:11 +0100

    Create context and initialize OpenGL

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
-};
+extern crate gl;
+
+use glutin::event::{Event, WindowEvent};
+use glutin::event_loop::{ControlFlow, EventLoop};
+use glutin::window::WindowBuilder;
+use glutin::ContextBuilder;
 
 fn main() {
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    let el = EventLoop::new();
+    let wb = WindowBuilder::new().with_title("A fantastic window!");
 
-    event_loop.run(move |event, _, control_flow| {
+    let windowed_context = ContextBuilder::new().build_windowed(wb, &el).unwrap();
+    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    let gl_context = windowed_context.context();
+
+    println!(
+        "Pixel format of the window's GL context: {:?}",
+        windowed_context.get_pixel_format()
+    );
+
+    gl::load_with(|ptr| gl_context.get_proc_address(ptr) as *const _);
+    unsafe {
+        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
+    }
+
+    el.run(move |event, _, control_flow| {
+        println!("{:?}", event);
         *control_flow = ControlFlow::Wait;
 
         match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                window_id,
-            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            Event::LoopDestroyed => return,
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(physical_size) => windowed_context.resize(physical_size),
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                _ => (),
+            },
+            Event::RedrawRequested(_) => {
+                unsafe {
+                    gl::Clear(gl::COLOR_BUFFER_BIT);
+                }
+                windowed_context.swap_buffers().unwrap();
+            }
             _ => (),
         }
     });
