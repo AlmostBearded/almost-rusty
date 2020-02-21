eda5b8248fdfa8f48b21e93b9f49dd1f7792a2e4
Peter Oberrauner <thealmostbearded@gmail.com>
2020-02-21 13:59:06 +0100

    Improve error handling readability

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use std::{
+    error::Error,
     fs,
     path::{Path, PathBuf},
     vec::Vec,
@@ -6,31 +7,34 @@ use std::{
 
 use walkdir::WalkDir;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let mut paths = Vec::<PathBuf>::new();
 
     // find all asset file paths
     for entry in WalkDir::new("assets") {
-        let entry = entry.unwrap();
+        let entry = entry?;
         let path = entry.path();
         paths.push(path.to_path_buf());
     }
 
     // rerun build script if asset files change
     for path in &paths {
-        println!("cargo:rerun-if-changed={}", path.to_str().unwrap());
+        let path = path
+            .to_str()
+            .expect("Paths may only contain UTF-8 characters");
+        println!("cargo:rerun-if-changed={}", path);
     }
 
     // clear modules
-    fs::remove_dir_all(Path::new("src/assets")).unwrap();
+    fs::remove_dir_all(Path::new("src/assets"))?;
 
     // create modules
     let dirs = paths.iter().filter(|&p| p.is_dir());
     for dir in dirs {
         // create module source
         let mut modrs_source = String::new();
-        for entry in fs::read_dir(&dir).unwrap() {
-            let entry = entry.unwrap();
+        for entry in fs::read_dir(&dir)? {
+            let entry = entry?;
             let path = entry.path();
             let file_name = path.file_name().unwrap().to_str().unwrap();
             if path.is_dir() {
@@ -45,10 +49,11 @@ fn main() {
 
         // create module folder
         let dir = Path::new("src").join(&dir);
-        fs::create_dir_all(&dir).unwrap();
+        fs::create_dir_all(&dir)?;
 
         // create module file
         let modrs_path = dir.join("mod.rs");
-        fs::write(modrs_path, modrs_source).unwrap();
+        fs::write(modrs_path, modrs_source)?;
     }
+    Ok(())
 }
